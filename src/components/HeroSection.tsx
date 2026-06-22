286324354c9c650da5467eccd2556b438ab82dcd
Lahiru.Dev <lahiru.rajakruna.dev@gmail.com>
2026-06-22 22:14:11 +0530

    2026/06/22 - 22:00

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,9 +2,12 @@ import { createEffect } from "solid-js";
 
 export default function HeroSection() {
   return (
-    <section class="relative w-full h-screen select-none mh:overflow-hidden">
+    <section
+      id="section-1"
+      class="relative w-full h-screen select-none mh:overflow-hidden"
+    >
       <div class="relative w-full h-screen p-4 flex flex-col items-stretch justify-center text-white bg-transparent perspective-distant perspective-origin-center">
-        <div id="monument" class="bg-image"></div>
+        {/* <div id="monument" class="bg-image"></div> */}
         <div id="angle" class="bg-image"></div>
         <div id="devil" class="bg-image"></div>
         {/* <Design /> */}
@@ -18,7 +21,7 @@ export default function HeroSection() {
 
 function H1() {
   return (
-    <h1 class="absolute z-[-3] mh:z-[8] top-1/2 mh:top-0 left-1/2 mh:left-4 -translate-x-1/2 mh:translate-x-0 -translate-y-[350%] mh:translate-y-0 translate-z-[-500px] mh:translate-z-[0px] transform-3d mb-12 text-6xl mh:text-4xl lg:text-7xl text-blue-950 font-bold text-center text-shadow-[0px_1px_5px_hsla(260,5%,1%,.8)] pointer-events-none">
+    <h1 class="absolute z-[-3] mh:z-[8] top-1/2 mh:top-0 left-1/2 mh:left-4 -translate-x-1/2 mh:translate-x-0 -translate-y-[350%] mh:translate-y-0 translate-z-[-500px] mh:translate-z-[0px] transform-3d mb-12 text-6xl mh:text-4xl lg:text-7xl text-blue-950 font-bold text-center text-shadow-[0px_1px_5px_hsla(260,5%,1%,.8)] pointer-events-none opacity-0">
       <span class="mh:hidden tracking-tighter">Shan Art</span>
       <br class="" />
       <span class="mh:hidden tracking-tighter">Advertising</span>
@@ -30,10 +33,10 @@ function H1() {
 function Text() {
   return (
     <div class="absolute z-[8] top-1/2 mh:top-5/12 lg:top-3/12 left-1/2 mh:left-4 -translate-x-1/2 mh:translate-x-0 translate-y-[-30%] w-full mh:w-1/2 lg:w-1/2 px-4 mh:p-0 text-black text-center mh:text-left lg:text-4xl lg:font-thin">
-      <p class="mb-4">
+      <p class="mb-4 opacity-0">
         Best and the most popular advertising agency in Anuradhapura
       </p>
-      <p class="mb-4">
+      <p class="mb-4 opacity-0">
         Affordable prices, House for most type of graphics, desigins and
         advertisings
       </p>
@@ -45,9 +48,11 @@ function CTA() {
   return (
     <a
       href="tel:+94741288437"
-      class="absolute z-[6] bottom-2/12 lg:bottom-4 left-1/2 mh:right-4 mh:left-auto lg:right-0 -translate-x-1/2 mh:translate-x-0 -translate-y-1/2 btn btn-primary w-11/12 mh:w-48 lg:w-78 bg-red-200 border-2 border-red-500 text-red-500"
+      class="absolute z-[6] bottom-2/12 lg:bottom-4 left-1/2 mh:right-4 mh:left-auto lg:right-0 -translate-x-1/2 mh:translate-x-0 -translate-y-1/2 btn btn-ghost w-11/12 mh:w-48 lg:w-78 p-0.5 bg-linear-120 from-amber-400 via-amber-100 to-amber-600 opacity-0"
     >
-      Contact Us
+      <p class="w-full h-full bg-white flex flex-col items-center justify-center text-amber-400">
+        Contact Us
+      </p>
     </a>
   );
 }
